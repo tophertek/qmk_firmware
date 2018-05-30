9d82eddd81a7c7283eb0a24f027ab1f2b3f8e0e0
tophertek <cweiss88@gmail.com>
2018-05-30 16:32:28 -0700

    based on dudeofawesome with space cadet shift and FN/ESC

--- keyboards/whitefox/keymaps/tophertek/keymap.c
+++ keyboards/whitefox/keymaps/tophertek/keymap.c
@@ -16,9 +16,6 @@ enum whitefox_keycodes {
   FUNC,
 };
 
-// Custom macros
-#define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
-
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* QWERTY (Default Layer)
      * ,---------------------------------------------------------------.
@@ -26,7 +23,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|Del|
      * |---------------------------------------------------------------|
-     * |Cl/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
+     * |Fn/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |PgU|
      * |---------------------------------------------------------------|
      * |Shif|   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |PgD|
      * |---------------------------------------------------------------|
@@ -36,9 +33,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = KEYMAP( \
       KC_ESC,           KC_1,   KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_PSCR, \
       KC_TAB,           KC_Q,   KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,        KC_DEL, \
-      CTL_ESC,KC_A,   KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,         KC_PGUP,\
+      LT(_FUNC,KC_ESC),KC_A,   KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,         KC_PGUP,\
       KC_LSPO,          KC_NUBS,KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,        KC_UP,  KC_PGDN,\
-      KC_LCTL,          KC_LALT,KC_LGUI,                 KC_SPC,             KC_RGUI,_FUNC,KC_RCTL,        KC_LEFT,KC_DOWN,KC_RGHT \
+      KC_LCTL,          KC_LALT,KC_LGUI,                 KC_SPC,             KC_RGUI,KC_RALT,KC_RCTL,        KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Workman
      * ,---------------------------------------------------------------.
@@ -57,7 +54,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,           KC_1,   KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_PSCR, \
       KC_TAB,           KC_Q,   KC_D,    KC_R, KC_W, KC_B, KC_J, KC_F, KC_U, KC_P,   KC_SCLN,KC_LBRC,KC_RBRC,KC_BSPC,        KC_DEL, \
       LT(_FUNC,KC_CAPS),KC_A,   KC_S,    KC_H, KC_T, KC_G, KC_Y, KC_N, KC_E, KC_O,   KC_I,   KC_QUOT,KC_NUHS,KC_ENT,         KC_PGUP,\
-      KC_LSFT,          KC_NUBS,KC_Z,    KC_X, KC_M, KC_C, KC_V, KC_K, KC_L, KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  KC_PGDN,\
+      KC_LSPO,          KC_NUBS,KC_Z,    KC_X, KC_M, KC_C, KC_V, KC_K, KC_L, KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,        KC_UP,  KC_PGDN,\
       KC_LCTL,          KC_LALT,KC_LGUI,                 KC_SPC,             KC_RGUI,KC_RALT,KC_RCTL,        KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Dvorak
@@ -77,7 +74,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,           KC_1,   KC_2,    KC_3,   KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9, KC_0, KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_PSCR, \
       KC_TAB,           KC_QUOT,KC_COMM, KC_DOT, KC_P,  KC_Y,  KC_F,  KC_G,  KC_C,  KC_R, KC_L, KC_SLSH,KC_EQL, KC_BSPC,        KC_DEL, \
       LT(_FUNC,KC_CAPS),KC_A,   KC_O,    KC_E,   KC_U,  KC_I,  KC_D,  KC_H,  KC_T,  KC_N, KC_S, KC_MINS,KC_NUHS,KC_ENT,         KC_PGUP,\
-      KC_LSFT,          KC_NUBS,KC_SCLN, KC_Q,   KC_J,  KC_K,  KC_X,  KC_B,  KC_M,  KC_W, KC_V, KC_Z,KC_RSFT,        KC_UP,  KC_PGDN,\
+      KC_LSPO,          KC_NUBS,KC_SCLN, KC_Q,   KC_J,  KC_K,  KC_X,  KC_B,  KC_M,  KC_W, KC_V, KC_Z,KC_RSPC,        KC_UP,  KC_PGDN,\
       KC_LCTL,          KC_LALT,KC_LGUI,                 KC_SPC,             KC_RGUI,KC_RALT,KC_RCTL,        KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Colemak
@@ -97,7 +94,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,           KC_1,   KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV, KC_PSCR, \
       KC_TAB,           KC_Q,   KC_W,    KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y,   KC_SCLN,KC_LBRC,KC_RBRC,KC_BSPC,        KC_DEL, \
       LT(_FUNC,KC_CAPS),KC_A,   KC_R,    KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I,   KC_O,   KC_QUOT,KC_NUHS,KC_ENT,         KC_PGUP,\
-      KC_LSFT,          KC_NUBS,KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  KC_PGDN,\
+      KC_LSPO,          KC_NUBS,KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM,KC_DOT, KC_SLSH,KC_RSPC,        KC_UP,  KC_PGDN,\
       KC_LCTL,          KC_LALT,KC_LGUI,                 KC_SPC,             KC_RGUI,KC_RALT,KC_RCTL,        KC_LEFT,KC_DOWN,KC_RGHT \
     ),
     /* Function
@@ -117,7 +114,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,_______,\
       _______,KC_WH_D,KC_BTN2,KC_MS_U,KC_BTN1,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,\
       _______,KC_WH_U,KC_MS_L,KC_MS_D,KC_MS_R,AG_NORM,AG_SWAP,QWERTY, WORKMAN,DVORAK, COLEMAK,_______,_______,_______,        _______,\
-      _______,_______,KC_WH_L,KC_BTN3,KC_WH_R,_______,_______,_______,_______,_______,_______,_______,_______,        KC_VOLU,_______,\
+      KC_HOME,_______,KC_WH_L,KC_BTN3,KC_WH_R,_______,_______,_______,_______,_______,_______,_______,KC_END,         KC_VOLU,_______,\
       _______,_______,_______,                   KC_MPLY,                     _______,_______,_______,        KC_MPRV,KC_VOLD,KC_MNXT \
     ),
 };
