0bbe2716ae16e5bd30a4bed04fb9743f98673070
tophertek <cweiss88@gmail.com>
2018-08-03 16:10:45 -0700

    Changed R3C1 to Esc/Ctrl on all layers

--- keyboards/preonic/keymaps/tophertek/keymap.c
+++ keyboards/preonic/keymaps/tophertek/keymap.c
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_TILD, _______, KC_UP,   _______, KC_AT,   KC_PGUP, KC_INS,  KC_SCLN, KC_P7,   KC_P8,   KC_P9,   KC_DEL,  \
-  KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ASTR, KC_HOME,  KC_END, KC_COLN, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
+  CTL_ESC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_ASTR, KC_HOME,  KC_END, KC_COLN, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, \
   _______, KC_LPRN, KC_RPRN, KC_EXLM, KC_HASH, KC_PGDN, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, KC_PENT  \
 ),
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,  KC_PGUP, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_DEL,  \
-  KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_HOME, KC_END,  KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, \
+  CTL_ESC,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_HOME, KC_END,  KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_PGDN, KC_BSLS, KC_MPRV, KC_LBRC, KC_RBRC, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
